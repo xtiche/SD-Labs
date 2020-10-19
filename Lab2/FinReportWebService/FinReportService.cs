73023ca9cddc3baedec08a4edae80c5ffddecf83
xtiche <xtiche@gmail.com>
2020-10-19 22:37:50 +0300

    Fix dir of solution

---
+++ Lab2/FinReportWebService/FinReportService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Web.Services;
+using System.Xml.Serialization;
+
+namespace FinReportWebService
+{
+
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [WebService(Description = "Фин. отчеты", Namespace = XmlNS)]
+    public class FinReportService : WebService
+    {
+        public const string XmlNS = "http://asmx.habrahabr.ru/";
+
+        [WebMethod(Description = "Получение списка ID отчетов по периоду")]
+        public GetReportIdArrayResult GetReportIdArray(GetReportIdArrayArg arg)
+        {
+            return new GetReportIdArrayResult()
+            {
+                ReportIdArray = new int[] { 357, 358, 360, 361 }
+            };
+        }
+
+        [WebMethod(Description = "Получение отчета по ID")]
+        public GetReportResult GetReport(GetReportArg arg)
+        {
+            return new GetReportResult()
+            {
+                Report = new FinReport
+                {
+                    ReportID = arg.ReportID,
+                    Date = new DateTime(2015, 03, 15),
+                    Info = getReportInfo(arg.ReportID)
+                }
+            };
+        }
+
+        private string getReportInfo(int reportID)
+        {
+            return "ReportID = " + reportID;
+        }
+    }
+
+
+    //    [Serializable]
+    //    [XmlType(Namespace = FinReportService.XmlNS)]
+    public class FinReport
+    {
+        public int ReportID { get; set; }
+        public DateTime Date { get; set; }
+        public string Info { get; set; }
+    }
+
+    public class GetReportIdArrayArg
+    {
+        public DateTime DateBegin { get; set; }
+        public DateTime DateEnd { get; set; }
+    }
+
+    public class GetReportIdArrayResult
+    {
+        public int[] ReportIdArray { get; set; }
+    }
+
+    public class GetReportArg
+    {
+        public int ReportID { get; set; }
+    }
+
+    public class GetReportResult
+    {
+        public FinReport Report { get; set; }
+    }
+}
