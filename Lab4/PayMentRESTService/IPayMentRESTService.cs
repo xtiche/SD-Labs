9a24251cc1c6ca7de9bd2981a35f50aa2fbe4e57
xtiche <xtiche@gmail.com>
2020-10-25 13:37:21 +0200

    Added Lab 4

---
+++ Lab4/PayMentRESTService/IPayMentRESTService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PayMentRESTService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IPayMentRESTService" in both code and config file together.
+    [ServiceContract]
+    public interface IPayMentRESTService
+    {
+        [OperationContract]
+        [WebInvoke(
+            Method = "GET",
+            UriTemplate = "/PayBill/{PayId}",
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        string PayBill(string PayId);
+    }
+}
---
+++ Lab4/PayMentRESTService/PayMentRESTService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PayMentRESTService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "PayMentRESTService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select PayMentRESTService.svc or PayMentRESTService.svc.cs at the Solution Explorer and start debugging.
+    public class PayMentRESTService : IPayMentRESTService
+    {
+        public string PayBill(string PayId)
+        {
+            return "Transaction having PayId " + PayId + " was successful";
+        }
+    }
+}
